5f3dce77c689046ba090f29a1aa79db375bb99b4
jovitor03 <joao.castro03@gmail.com>
2023-03-20 20:12:13 +0000

    Deleted .zip

---
+++ OficinaEDA/contagemLinhas.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <fstream>
+#include <string>
+#include "contagemLinhas.h"
+
+using namespace std;
+
+int contarMarcas() {
+	string file = "marcas.txt";
+	ifstream fileMarcas(file);
+	string marca;
+	int num_marcas = 0;
+	if (fileMarcas.is_open()) {
+		while (getline(fileMarcas, marca)) {
+			num_marcas++;
+		}
+	}
+	return num_marcas;
+}
+
+int contarModelos() {
+	string file = "modelos.txt";
+	ifstream fileModelos(file);
+	string modelo;
+	int num_modelos = 0;
+	if (fileModelos.is_open()) {
+		while (getline(fileModelos, modelo)) {
+			num_modelos++;
+		}
+	}
+	return num_modelos;
+}
